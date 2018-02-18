4bb5f6a3baad2c3f70996b7bff6b8640d7e80c4c
amaruk <amaruk@163.com>
2018-02-18 15:51:26 +0100

    Add README.md and update main.cpp.

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is used to evaluate JetBrains Clion IDE for C++ projects.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// To reset CMake cache: Tools -> CMake -> Reset cache and reload project
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
