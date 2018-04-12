db4107d18f7802103a252fa5970751aa51839727
amaruk <amaruk@163.com>
2018-04-12 08:21:02 +0200

    Update CMake configuration.

--- Solution/ProjExe/Sources/MyExe.cpp
+++ Solution/ProjExe/Sources/MyExe.cpp
@@ -6,8 +6,14 @@ int main() {
   ClassInExe classInExe{};
   ClassInLib classInLib{};
   ClassInThirdParty classInThridParty{};
+
 #ifdef USE_DUMMY_LIB
 #error "OMG! Shouldn't use the dummy lib!"
 #endif
+
+#ifdef DEBUG_OUTPUT
+  std::cout << "DEBUG OUTPUT ENABLED!\n";
+#endif
+
     return 0;
 }
